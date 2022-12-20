4759c4fb123bcded1d8809d47e4b15eca3849ed1
MansurIsakov <mansurisakov.dev@gmail.com>
2022-12-20 21:46:55 +0500

    feat: add cutrom picker

--- projects/forms-playground/src/app/playgrounds/reactive-forms/validators/ban-words.validator.ts
+++ projects/forms-playground/src/app/playgrounds/reactive-forms/validators/ban-words.validator.ts
@@ -3,7 +3,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 export function banWords(bannedWords: string[] = []): ValidatorFn {
   return (control: AbstractControl<string | null>): ValidationErrors | null => {
     const foundBannedWord = bannedWords.find(
-      (word) => word.toLowerCase() === control.value?.toLowerCase()
+      (word) => word?.toLowerCase() === control.value?.toLowerCase()
     );
 
     return !foundBannedWord
